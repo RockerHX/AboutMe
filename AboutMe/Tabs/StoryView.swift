9d613d0090caa102aa23154c6ef34e1b560a832b
RockerHX <rockerhx@gmail.com>
2022-05-10 15:26:49 +0800

    Add[View]: StoryView

---
+++ AboutMe/Tabs/StoryView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  AboutMe
+//
+//  Created by Andy Shaw on 2022/5/10.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            ScrollView {
+                Text(information.story)
+                    .font(.body)
+                    .padding()
+            }
+        }
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
